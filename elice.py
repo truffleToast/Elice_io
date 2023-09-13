f001e92bd3bcf0b8ed372e50a33329973c5e6f38
truffleToast <kimchi98125@gmail.com>
2023-09-13 18:49:49 +0900

    엘리스 유지보수

--- elice.py
+++ elice.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 import time
-import datetime as dt
+
 
 # 파일 경로를 raw 문자열로 작성하고 백슬래시를 이스케이프 문자로 처리
 # 자신의 파일 절대 경로 지정
@@ -21,7 +21,7 @@ My_id = json_data['elice']['id']
 My_pw = json_data['elice']['pw']
 
 chulsuk_button_class = ".css-1i9rc1m"
-tuisil_button_class = ".css_1i9rc1m"
+
 
 # WebDriver 초기화
 driver = webdriver.Chrome()
@@ -44,27 +44,24 @@ try:
     id_input.send_keys(My_id)
     pw_input.send_keys(My_pw)
     pw_input.send_keys(Keys.ENTER)
-
+    print("로그인 완료")
     time.sleep(3)
-    present_time = dt.datetime.now()  # 현재 시간 추출
 
-    if 8 <= present_time.hour <= 9:  # 8시 ~ 9시 사이
-        button_class = chulsuk_button_class
-    elif 17 <= present_time.hour <= 19:  # 17시 이후부터 19시 사이
-        button_class = tuisil_button_class
-    else:
-        print('출석/퇴실이 가능한 시간이 아닙니다.')
-        time.sleep(2)
+    my_lecture = driver.find_element(By.CSS_SELECTOR, ".css-1rc2wrl")
+    time.sleep(2)
+    my_lecture = my_lecture.text
+    print(my_lecture)
+    if my_lecture != "Spring" : #spring이 아니면 종료
         driver.quit()
-
-    # 출석 버튼이 나타날 때까지 기다림
+    print("출석 버튼이 나타날 때까지 기다림")
     try:
         attendance_button = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, button_class))
+            EC.presence_of_element_located((By.CSS_SELECTOR, chulsuk_button_class))
         )
         attendance_button.click()
     except TimeoutException:
         print("출석 버튼이 발견되지 않았습니다.")
+        driver.quit()
 
 finally:
     # WebDriver 종료
